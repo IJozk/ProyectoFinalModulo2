f93705b7dd2aa4d321a3ceb2ad6fb49103ff2d95
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2025-11-24 23:22:12 -0300

    Se añade carrusel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,4 +6,38 @@
 
 // Daniel
 
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+const indicators = document.querySelectorAll(".indicator");
+let currentIndex = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+    indicators.forEach(i => i.classList.remove("active"));
+    indicators[currentIndex].classList.add("active");
+}
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
+indicators.forEach((indicator, i) => {
+    indicator.addEventListener("click", () => {
+        currentIndex = i;
+        updateCarousel();
+    });
+});
+
+
 // Elvis
