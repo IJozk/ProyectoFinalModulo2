97ff16e48ceb86adf2e80c2875fcf004c28b48ce
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2025-11-25 19:01:34 -0300

    Terminado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,47 @@ const boton = document.getElementById("boton-contacto")
 boton.addEventListener('click', function(event){
     event.preventDefault();
 
+    const input1 = document.getElementById("validationCustom01").value
+    const input2 = document.getElementById("validationCustom02").value
+    const input3 = document.getElementById("validationCustom03").value
+    const input4 = document.getElementById("validationCustom04").value
+    const input5 = document.getElementById("mensaje-contacto").value
+    const inputemail = document.getElementById("validationCustomUsername").value
+
+    const msjval1 = document.getElementById("msjvalidation1")
+    const msjval2 = document.getElementById("msjvalidation2")
+    const msjval3 = document.getElementById("msjvalidation3")
+    const msjval4 = document.getElementById("msjvalidation4")
+    const msjval5 = document.getElementById("msjvalidation5")
+    const msjvalemail = document.getElementById("msjvalidationemail")
+
+    function validar(input, msjval){
+        if(input == ""){
+            msjval.innerHTML = "<p style= 'color: red;'> Malo </p>";
+            return false;
+        }
+        else{
+            msjval.innerHTML = "<p style= 'color: green;'> Bueno </p>";
+            return true;
+        }
+
+    }
+
+    const val1 = validar(input1, msjval1)
+    const val2 =validar(input2, msjval2)
+    const val3 =validar(input3, msjval3)
+    const val4 =validar(input4, msjval4)
+    const val5 =validar(input5, msjval5)
+    const val6 =validar(inputemail, msjvalemail)
+
+    if (val1 && val2 && val3 && val4 && val5 && val6){
+        alert("Mensaje Enviado")
+    } 
+    else{
+        alert("No se pudo enviar el formulario")
+    }
     
+
 })
 
 
